19e3a1ea3bf83be82216bf6be3be609e4c9556ee
SergiyDB <sergiy.dmitrievich@fractalsoft.ec>
2024-07-25 14:08:51 -0500

    Small code formating corrections

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="fullscreen row">
+
     <div style="padding-top: 5vh" class="col-12 col-md-6 column items-center">
       <router-view />
     </div>
-    <div style="padding-top: 30vh" class="col-12 col-md-6 covered column q-px-xl">
-      <p class="text-h2 text-white text-bold">Welcome to</p>
-      <p class="text-h4 text-white text-bold">P3T3's Learning Management System</p>
-      <p class="text-h6 text-white text-uppercase">AI-personalized language learning</p>
+
+    <div style="padding-top: 30vh" class="col-12 col-md-6 bg-primary text-white column q-px-xl">
+      <p class="text-h2 text-bold">Welcome to</p>
+      <p class="text-h4 text-bold">P3T3's Learning Management System</p>
+      <p class="text-h6 text-uppercase">AI-personalized language learning</p>
     </div>
   </div>
 </template>
@@ -20,7 +22,4 @@ defineOptions({
 </script>
 
 <style lang="scss" scoped>
-.covered {
-  background-color: #133f3e;
-}
 </style>
